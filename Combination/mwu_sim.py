7ceb5bed304c723abe394f0e6c097815af18b40e
lana.atiya <lana.atiya@tum.de>
2026-06-08 13:27:31 +0200

    payoff matrix now changable

--- Combination/mwu_sim.py
+++ Combination/mwu_sim.py
@@ -1,8 +1,23 @@
-import math
-import random
 
 
-def mwu_step(score_A, score_B, opponent_conf_A, learning_speed, noise_A, noise_B):
-    r_A = opponent_conf_A + noise_A
-    r_B = (1 - opponent_conf_A) + noise_B
-    return score_A + learning_speed * r_A, score_B + learning_speed * r_B
+def mwu_step(score_A, score_B, opponent_conf_A, learning_speed, payoff_matrix, noise_A=0, noise_B=0):
+
+    pA = opponent_conf_A
+    pB = 1 - opponent_conf_A
+
+    # Expected payoff of action A
+    r_A = (
+        pA * payoff_matrix[0][0] +
+        pB * payoff_matrix[0][1]
+    ) + noise_A
+
+    # Expected payoff of action B
+    r_B = (
+        pA * payoff_matrix[1][0] +
+        pB * payoff_matrix[1][1]
+    ) + noise_B
+
+    return (
+        score_A + learning_speed * r_A,
+        score_B + learning_speed * r_B
+    )
--- Combination/pgd_sim.py
+++ Combination/pgd_sim.py
@@ -1,7 +1,21 @@
-import random
 
-def pgd_step(conf_A, opponent_conf_A, learning_speed, noise_A, noise_B):
-    r_A = opponent_conf_A + noise_A
-    r_B = (1 - opponent_conf_A) + noise_B
+def pgd_step(conf_A, opponent_conf_A, learning_speed, payoff_matrix, noise_A=0, noise_B=0):
+
+    pA = opponent_conf_A
+    pB = 1 - opponent_conf_A
+
+    # Expected payoff of choosing A
+    r_A = (
+        pA * payoff_matrix[0][0] +
+        pB * payoff_matrix[0][1]
+    ) + noise_A
+
+    # Expected payoff of choosing B
+    r_B = (
+        pA * payoff_matrix[1][0] +
+        pB * payoff_matrix[1][1]
+    ) + noise_B
+
     gradient = r_A - r_B
+
     return max(0, min(1, conf_A + learning_speed * gradient))
